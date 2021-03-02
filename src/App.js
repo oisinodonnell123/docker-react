df95ee85beac41f150a57d84da272a252ece56c2
Oisin <oisinodonnell6@gmail.com>
2021-03-02 14:01:05 +0000

    app.js changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
           Hey Helen, how is your day going? 
         </p>
         <a
@@ -18,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          =======
         </a>
       </header>
     </div>
